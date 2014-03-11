e0a5f6ed56057ae63a61f47f8aedfded572de49f
David Stark <david.stark@zarkonnen.com>
2014-03-11 20:20:03 +0100

    Implemented 1st detail.

--- src/com/zarkonnen/cyberpunk/behave/Detail.java
+++ src/com/zarkonnen/cyberpunk/behave/Detail.java
@@ -1,14 +1,27 @@
 package com.zarkonnen.cyberpunk.behave;
 
 import com.zarkonnen.cyberpunk.ItemType;
+import static com.zarkonnen.cyberpunk.ItemType.*;
+import static com.zarkonnen.cyberpunk.TileType.*;
 import com.zarkonnen.cyberpunk.Person;
+import com.zarkonnen.cyberpunk.TileType;
+import com.zarkonnen.cyberpunk.interaction.Buy;
+import com.zarkonnen.cyberpunk.interaction.MoveToType;
+import com.zarkonnen.cyberpunk.interaction.TakeDrug;
 import java.util.EnumSet;
 import java.util.Random;
 
 public enum Detail {
-	;
+	UPPER_ADDICT(" They have a nervous look about them.", EnumSet.of(UPPERS, EXPERIMENTAL_UPPERS), EnumSet.noneOf(ItemType.class), EnumSet.of(UPPERS)) {
+		@Override
+		public void install(Person p, Random r) {
+			p.behave(TakeDrug.class).sober();
+			p.behave(MoveToType.class).moveTo(DRUG_DEN);
+			p.behave(Buy.class).item(UPPERS, EXPERIMENTAL_UPPERS);
+		}
+	};
 
-	/*private Detail(String description, EnumSet<ItemType> buys, EnumSet<ItemType> sells, EnumSet<ItemType> equipment) {
+	private Detail(String description, EnumSet<ItemType> buys, EnumSet<ItemType> sells, EnumSet<ItemType> equipment) {
 		this.description = description;
 		this.buys = buys;
 		this.sells = sells;
@@ -20,5 +33,5 @@ public enum Detail {
 	public final EnumSet<ItemType> sells;
 	public final EnumSet<ItemType> equipment;
 	
-	public abstract void install(Person p, Random r);*/
+	public abstract void install(Person p, Random r);
 }
